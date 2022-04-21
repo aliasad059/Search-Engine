10e15f515217f5c6801f34e86577e4d90fb69e24
a.asad <aliasad059@gmail.com>
2022-04-21 14:33:50 +0430

    feat: preprocessing step added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from hazm import Normalizer, word_tokenize, stopwords_list, Stemmer
+import numpy as np
 import pandas as pd
+import re
+from string import punctuation
 
 
 def load_df(file_name):
@@ -8,6 +12,44 @@ def load_df(file_name):
     return pd.read_json(file_name).transpose()
 
 
+def preprocess_df(df, verbose=False):
+    """
+    Preprocesses a dataframe.
+    normalizes and tokenizes the text.
+    removes stopwords, punctuation, urls ,and stemming.
+    """
+    if verbose:
+        print('Removing punctuations...')
+    df['content'] = df['content'].apply(lambda x: re.sub(f'[{punctuation}؟،٪×÷»«]+', '', x))
+    if verbose:
+        print('Removing URLs...')
+    df['content'] = df['content'].apply(lambda x: re.sub(r'http\S+', '', x))
+    if verbose:
+        print('Normalizing...')
+    df['content'] = df['content'].apply(lambda x: Normalizer().normalize(x))
+    if verbose:
+        print('Tokenizing...')
+    df['content'] = df['content'].apply(lambda x: word_tokenize(x))
+    if verbose:
+        print('Removing stopwords...')
+    df['content'] = df['content'].apply(lambda x: [w for w in x if w not in stopwords_list()])
+    if verbose:
+        print('Stemming...')
+    df['content'] = df['content'].apply(lambda x: [Stemmer().stem(w) for w in x])
+    if verbose:
+        print('Joining...')
+    df['content'] = df['content'].apply(lambda x: ' '.join(x))
+    if verbose:
+        print('Done.')
+    return df
+
+
 if __name__ == '__main__':
     # Load dataframe
     df = load_df('data/raw_data.json')
+
+    # Preprocess dataframe
+    df = preprocess_df(df[0:5])
+
+    # Save dataframe
+    # df.to_json('data/preprocessed_data.json')
