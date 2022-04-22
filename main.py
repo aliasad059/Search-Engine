320df59d0fbd5fd89c26cefce1feee43704536b2
a.asad <aliasad059@gmail.com>
2022-04-22 13:26:52 +0430

    feat: simple querying added

--- main.py
+++ main.py
@@ -12,50 +12,66 @@ def load_df(file_name):
     return pd.read_json(file_name).transpose()
 
 
-def preprocess_df(df, verbose=False):
+def preprocess_query(query):
     """
-    Preprocesses a dataframe.
+    Preprocesses a query.
+    normalizes and tokenizes the query.
+    removes stopwords, punctuation, urls ,and stemming.
+    """
+    query = re.sub(f'[{punctuation}؟،٪×÷»«]+', '', query)
+    query = re.sub(r'http\S+', '', query)
+    query = Normalizer().normalize(query)
+    query = word_tokenize(query)
+    query = [w for w in query if w not in stopwords_list()]
+    query = [Stemmer().stem(w) for w in query]
+    return ' '.join(query)
+
+
+def preprocess_df(df, column_name, verbose=False):
+    """
+    Preprocesses a dataframe column.
     normalizes and tokenizes the text.
     removes stopwords, punctuation, urls ,and stemming.
     """
     if verbose:
-        print('Removing punctuations...')
-    df['content'] = df['content'].apply(lambda x: re.sub(f'[{punctuation}؟،٪×÷»«]+', '', x))
-    if verbose:
         print('Removing URLs...')
-    df['content'] = df['content'].apply(lambda x: re.sub(r'http\S+', '', x))
+    df[column_name] = df[column_name].apply(lambda x: re.sub(r'http\S+', '', x))
+    if verbose:
+        print('Removing punctuations...')
+    df[column_name] = df[column_name].apply(lambda x: re.sub(f'[{punctuation}؟،٪×÷»«]+', '', x))
     if verbose:
         print('Normalizing...')
-    df['content'] = df['content'].apply(lambda x: Normalizer().normalize(x))
+    df[column_name] = df[column_name].apply(lambda x: Normalizer().normalize(x))
     if verbose:
         print('Tokenizing...')
-    df['content'] = df['content'].apply(lambda x: word_tokenize(x))
+    df[column_name] = df[column_name].apply(lambda x: word_tokenize(x))
     if verbose:
         print('Removing stopwords...')
-    df['content'] = df['content'].apply(lambda x: [w for w in x if w not in stopwords_list()])
+    df[column_name] = df[column_name].apply(lambda x: [w for w in x if w not in stopwords_list()])
     if verbose:
         print('Stemming...')
-    df['content'] = df['content'].apply(lambda x: [Stemmer().stem(w) for w in x])
+    df[column_name] = df[column_name].apply(lambda x: [Stemmer().stem(w) for w in x])
     if verbose:
         print('Joining...')
-    df['content'] = df['content'].apply(lambda x: ' '.join(x))
+    df[column_name] = df[column_name].apply(lambda x: ' '.join(x))
     if verbose:
         print('Done.')
     return df
 
 
-def create_index_dict(df):
+def create_index_dict(df, column_name):
     """
     Creates a dictionary of word indexes.
     """
     word_index = {}
     for i, row in df.iterrows():
-        for word in row['content'].split():
+        for word in row[column_name].split():
             if word not in word_index:
-                word_index[word] = {-1: 1}  # -1 holds count of the word in all documents
+                word_index[word] = {}
+                word_index[word]['count'] = 1  # holds count of the word in all documents
                 word_index[word][i] = 1  # i holds count of the word in document i
             else:
-                word_index[word][-1] += 1
+                word_index[word]['count'] += 1
                 if i not in word_index[word]:
                     word_index[word][i] = 1
                 else:
@@ -63,16 +79,76 @@ def create_index_dict(df):
     return word_index
 
 
+def query_to_index(query, word_index, preprocessed=False):
+    """
+    Converts a query to a list of word indexes.
+    """
+    if not preprocessed:
+        query = preprocess_query(query)
+    return [word_index[word] for word in query.split()]
+
+
+def single_word_query(word, word_index):
+    """
+    Answers to a single word query and sorts the results.
+    """
+    if word not in word_index:
+        return []
+
+    posting_list = word_index[word]
+    result = sorted(posting_list, key=lambda x: posting_list[x], reverse=True)
+    return result
+
+
+def multiple_word_query(query, word_index):
+    """
+    Answers to a multiple word query and sorts the results.
+    """
+    posting_lists = query_to_index(query, word_index)
+    result = list(posting_lists[0].keys())
+    for posting_list in posting_lists[1:]:
+        index = list(posting_list.keys())
+        result = intersect_indexes(result, index)
+    ranked_result = np.zeros(len(result))
+    for p in posting_lists:
+        ranked_result += [p[i] for i in result]
+    return [x for y, x in sorted(zip(ranked_result, result), reverse=True)]
+
+
+def intersect_indexes(index1, index2):
+    """
+    Intersects two sorted indexes.
+    """
+    result = []
+    i = 0
+    j = 0
+    while i < len(index1) and j < len(index2):
+        if index1[i] == index2[j]:
+            result.append(index1[i])
+            i += 1
+            j += 1
+        elif index1[i] < index2[j]:
+            i += 1
+        else:
+            j += 1
+    return result
+
+
 if __name__ == '__main__':
     # Load dataframe
     df = load_df('data/raw_data.json')
 
     # Preprocess dataframe
-    df = preprocess_df(df[0:5])
+    df = preprocess_df(df[0:5], column_name='content')
 
     # Save dataframe
     # df.to_json('data/preprocessed_data.json')
 
     # Create index dictionary
-    word_index = create_index_dict(df)
-    print(word_index['فوتبال'])
+    word_index = create_index_dict(df, column_name='content')
+
+    # Single word query
+    print(single_word_query('بسکتبال', word_index))
+
+    # Multiple word query
+    print(multiple_word_query('بسکتبال لیگ', word_index))
